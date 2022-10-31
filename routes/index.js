edb51c553659a26e9b4cda111eac9bf34541242f
Diego Mercado <diego.mercado1997@hotmail.com>
2022-10-31 00:22:13 -0300

    .

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,6 @@ const api = express.Router();
 const { userController, fiboController } = require("../controllers");
 
 api.get("/users", userController.getUsers);
-// api.get("/users/:id", userController.getOneUser);
 api.post("/users", userController.createUser);
 api.put("/users/:id", userController.updateUser);
 api.delete("/users/:id", userController.deleteUser);
